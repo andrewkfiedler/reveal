20fcd3d829e6b8cc7c52d71935e847e3d6f79647
Sam <samuelechu@umail.ucsb.edu>
2019-09-09 12:51:34 -0700

    Integrate filterTree functions into basic search

--- src/main/webapp/basic-search.js
+++ src/main/webapp/basic-search.js
@@ -2,43 +2,70 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 import Button from '@material-ui/core/Button'
-import Card from '@material-ui/core/Card'
-import CardContent from '@material-ui/core/CardContent'
+import Paper from '@material-ui/core/Paper'
 import TextField from '@material-ui/core/TextField'
+import Select from '@material-ui/core/Select'
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import FormLabel from '@material-ui/core/FormLabel'
+import InputLabel from '@material-ui/core/InputLabel'
+import MenuItem from '@material-ui/core/MenuItem'
 
 import { executeQuery } from './intrigue-api/lib/cache'
 
-const TextSearch = props => {
-  const [filterTree, setFilterTree] = React.useState({
-    type: 'ILIKE',
-    property: 'anyText',
-    value: '',
-  })
+import exampleFilterTree from './filterTree.json'
+import { toFilterTree, fromFilterTree } from './basic-search-helper'
+
+//console.log(fromFilterTree(exampleFilterTree));
+const TextSearch = ({ filterTree, setFilterTree }) => {
+  // const [filterTree, setFilterTree] = React.useState({
+  //   type: "ILIKE",
+  //   property: "anyText",
+  //   value: ""
+  // });
 
   return (
-    <CardContent>
-      <TextField
-        label="Text"
-        variant="outlined"
-        fullWidth
-        value={filterTree.value}
-        onChange={e => {
-          const value = e.target.value
-          setFilterTree({ ...filterTree, value })
-        }}
-      />
-
-      <Button
-        fullWidth
-        variant="contained"
-        color="primary"
-        onClick={() => props.onSearch(filterTree)}
-      >
-        Search
-      </Button>
-    </CardContent>
+    <TextField
+      label="Text"
+      variant="outlined"
+      fullWidth
+      value={filterTree.text}
+      onChange={e => {
+        const value = e.target.value
+        setFilterTree({ ...filterTree, text: value })
+      }}
+    />
   )
 }
+
+const SearchButton = props => (
+  <Button
+    fullWidth
+    variant="contained"
+    color="primary"
+    onClick={props.onSearch}
+  >
+    Search
+  </Button>
+)
+
+const TimeRange = props => (
+  <FormControl>
+    <InputLabel htmlFor="age-simple">Age</InputLabel>
+    <Select
+      value={10}
+      // onChange={(}
+      inputProps={{
+        name: 'age',
+        id: 'age-simple',
+      }}
+    >
+      <MenuItem value={10}>Ten</MenuItem>
+      <MenuItem value={20}>Twenty</MenuItem>
+      <MenuItem value={30}>Thirty</MenuItem>
+    </Select>
+  </FormControl>
+)
 
 const populateDefaultQuery = filterTree => ({
   // hello source to test error case
@@ -59,16 +86,23 @@ const populateDefaultQuery = filterTree => ({
 })
 
 export const BasicSearch = props => {
+  const [filterTree, setFilterTree] = React.useState(
+    fromFilterTree(exampleFilterTree)
+  )
+  console.log(exampleFilterTree)
+  console.log(fromFilterTree(exampleFilterTree))
+  console.log(filterTree)
+
   return (
-    <div style={{ maxWidth: 600, margin: '20px auto' }}>
-      <Card>
-        <TextSearch
-          onSearch={filterTree => {
-            props.onSearch(populateDefaultQuery(filterTree))
-          }}
-        />
-      </Card>
-    </div>
+    <Paper style={{ maxWidth: 600, margin: '20px auto' }}>
+      <TextSearch filterTree={filterTree} setFilterTree={setFilterTree} />
+      <TimeRange />
+      <SearchButton
+        onSearch={() => {
+          props.onSearch(populateDefaultQuery(toFilterTree(filterTree)))
+        }}
+      />
+    </Paper>
   )
 }
 
